9f1f713f1ce3636231d8164b36ebad03d31ebb3d
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-06-01 16:53:52 +0200

    Update TokenVesting.js

--- test/TokenVesting.js
+++ test/TokenVesting.js
@@ -118,10 +118,10 @@ describe("TokenVesting", function () {
       // check that the released amount is 10
       expect(vestingSchedule.released).to.be.equal(10);
 
-      // set current time after the end of the vesting
+      // set current time after the end of the vesting period
       await tokenVesting.setCurrentTime(baseTime + duration + 1);
 
-      // check that the vesting amount is 90
+      // check that the vested amount is 90
       expect(
         await tokenVesting
           .connect(beneficiary)
@@ -146,6 +146,26 @@ describe("TokenVesting", function () {
           .connect(beneficiary)
           .computeVestedAmount(vestingScheduleId)
       ).to.be.equal(0);
+
+      /*
+       * TEST SUMMARY
+       * deploy vesting contract
+       * send tokens to vesting contract
+       * create new vesting schedule (100 tokens)
+       * check that vested amount is 0
+       * set time to half the vesting period
+       * check that vested amount is half the total amount to vest (50 tokens)
+       * check that only beneficiary can try to release vested tokens
+       * check that beneficiary cannot release more than the vested amount
+       * release 10 tokens and check that a Transfer event is emitted with a value of 10
+       * check that the released amount is 10
+       * check that the vested amount is now 40
+       * set current time after the end of the vesting period
+       * check that the vested amount is 90 (100 - 10 released tokens)
+       * release all vested tokens (90)
+       * check that the number of released tokens is 100
+       * check that the vested amount is 0
+       */
     });
 
     it("Should compute vesting schedule index", async function () {
