36ab57c0f0b27aa497563b294bcd5cc00a609685
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-06-07 14:10:34 +0200

    enable optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,15 @@ require("solidity-coverage");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     goerli: goerliNetworkConfig(),
     bscTestnet: bscTestnetNetworkConfig(),
