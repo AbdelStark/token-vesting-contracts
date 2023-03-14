12f8222b9c23876ec65e19150225a9568e15b2f3
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-03-14 13:12:31 +0100

    wip

--- src/Token.sol
+++ src/Token.sol
@@ -2,14 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract Token is ERC20 {
     constructor(
         string memory name,
         string memory symbol,
         uint256 initialSupply
-    ) ERC20(name, symbol) {
+    ) ERC20(name, symbol, 18) {
         _mint(msg.sender, initialSupply);
     }
 }
